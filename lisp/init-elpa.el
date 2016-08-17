7732b266b0a00f755a2d0db4539ada6bc070cadb
Lauro Caetano <lauro.caetano@fyber.com>
2016-08-17 16:48:44 +0200

    Add require-package function
    Credits to https://github.com/purcell/emacs.d

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -3,6 +3,24 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives `("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+
+(package-initialize)
+
+;;; From https://github.com/purcell/emacs.d/blob/542344712dfbe5559de793abf57aaeb94e2c2dbd/lisp/init-elpa.el
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (if (boundp 'package-selected-packages)
+            ;; Record this as a package the user installed explicitly
+            (package-install package nil)
+          (package-install package))
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
 
 (provide 'init-elpa)
